0acfcf5f327b1a2e4a64c0a7380a4c33ced1f059
KeshavTandon <keshavtandon51@gmail.com>
2024-04-11 16:42:38 +0530

    Js Notes

---
+++ Demo.js
@@ -0,0 +1,86 @@
+// "use strict";
+// let x=1;
+// let s="1";
+// if(x===s)
+// {
+//     console.log("Hi");
+// }
+// else console.log("Hello");
+
+// function download(url,cb){
+//     console.log("Downloading content from url",url);
+//     setTimeout(function down(){
+//         console.log("Download completed");
+//         let content="ABSDEC";
+//         cb(content);
+//     },4000)
+// }
+
+// function writeFile(data,cb){
+//     console.log("Started writing",data);
+//     setTimeout(function write(){
+//         console.log("Completed writing");
+
+//     },3000);
+// }
+
+// class Product{
+//     // name;
+//     // price;
+//     // desc;
+
+//     constructor(a,b,c){
+//         this.name=a;
+//         this.price=b;
+//         this.desc=c;
+//     }
+
+//     display(){
+//         console.log(this);
+//     }
+
+// }
+
+// const p=new Product("iphone",100000,"It is brand new iphone");
+// p.display();
+
+// var name="Keshav";
+// function fn(){
+//     console.log(name1);
+//      var name1="Rahul";
+//      console.log(name);
+//      function gn(){
+//         var namee="NewName";
+//         console.log(name);
+//         console.log(name1);
+//         console.log(namee);
+//      }
+//      gn();
+// }
+// fn();
+// console.log(name);
+// console.log(namee);
+
+/*
+undefined
+keshav
+keshav
+rahul
+newName
+keshav
+
+*/
+
+// function hi(){
+//     "use strict";
+//     name="Keshav";
+// }
+// hi();
+// console.log(name);
+
+// console.log("hey");
+
+var date = new Date();
+console.log(date.getDate());
+console.log(date.getDay());
+console.log(date.getTime());
